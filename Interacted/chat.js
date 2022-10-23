45ab25fa1e906e1f940442d6a0f6601884270caa
DiamondSpaceGD <79640910+DiamondSpaceGD@users.noreply.github.com>
2022-10-23 10:23:50 +0700

    Interacted Update

---
+++ Interacted/chat.js
@@ -0,0 +1,10 @@
+var chat = [
+	"uwu",
+	"owo",
+	"*cuddle*",
+	"hug pls",
+	"do chu wike fuwwy uwu?",
+	"Am i adowabwe uwu to chu?",
+	"I wannya hug",
+	"Im actuawwy a weaw pewson~ nyot a bot qwq",
+];
